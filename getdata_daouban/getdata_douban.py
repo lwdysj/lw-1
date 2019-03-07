c476c01574084fb5ba8d7d1f1399b71a2367361c
lwdysj <1604547603@qq.com>
2019-03-07 11:32:41 +0800

    豆瓣评论爬取

--- getdata_daouban/getdata_douban.py
+++ getdata_daouban/getdata_douban.py
@@ -1,44 +1,51 @@
 #! /usr/bin/python3 env
 # -*- coding:utf-8 -*-'
+
 '''
-Created on 2019年3月6日
+Created on 2018年10月16日
 
 @author: liwei
 '''
-#爬取最好大学排行
+
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 import urllib.request
+from urllib.error import HTTPError, URLError
 from mylog import MyLog
-import re
-import codecs
-import random
+import re, codecs, random
 
 class Item(object):
-    paiming = None
     name = None
-    address = None
-    score = None
+    content = None
 
 class GetData(object):
     def __init__(self):
-        self.url = 'http://www.zuihaodaxue.com/zuihaodaxuepaiming2016.html'
+        self.url = 'https://movie.douban.com/subject/26266893/reviews?start='
         self.log = MyLog()
+        self.urls = self.get_urls()
         self.items = self.spider()
         self.pipelines()
         
-    def get_response(self):
-        #获取页面信息
+    def get_urls(self):
+        pages = 60
+        urls = []
+        for i in range(0, pages, 20):
+            url = self.url + str(i)
+            urls.append(url)
+            self.log.info(u'导入URL 成功')
+        return urls
+    
+    def get_response(self, url):
         flag = True
         ua = UserAgent()
         while flag:
-            with open('new3proxy.txt', 'r') as fp:
+            with open('new4proxy.txt', 'r') as fp:
                 lines = fp.readlines()
                 index = random.randint(1, len(lines))
                 proxys = 'https://' + lines[index-1]
             
             fakeHeaders = {'User-Agent':ua.random}
-            request = urllib.request.Request(self.url, headers=fakeHeaders)
+            request = urllib.request.Request(url, headers=fakeHeaders)
                 
             proxy = urllib.request.ProxyHandler({'https':proxys})
             opener = urllib.request.build_opener(proxy)
@@ -49,32 +56,30 @@ class GetData(object):
                 flag = False
                 self.log.info(u'导入URL: 成功')
                 return response
-            except:
+            except (HTTPError, URLError):
                 flag = True
                 self.log.error(u'导入URL: 失败')
         
     def spider(self):
-        #数据提取
         items = []
-        response = self.get_response()
-        soup = BeautifulSoup(response.read(), 'html.parser')
-        datas = soup.find('div', {'class': 'news-text'}).find_all('tr')
-        for data in datas[1:5]:
-            item = Item()
-            item.paihang = data.find_all('td')[0].text
-            item.name = data.find_all('td')[1].text
-            item.address = data.find_all('td')[2].text
-            item.score = data.find_all('td')[3].text
-            items.append(item)
-            self.log.info(u'获取%s信息: 成功' % item.name)
+        for url in self.urls:
+            response = self.get_response(url)
+            try:
+                item = Item()
+                soup = BeautifulSoup(response.read(), 'html.parser')
+                item.name = soup.find('a', {'class': 'name'}).text
+                item.content = soup.find('div', {'class': 'short-content'}).text
+                items.append(item)
+                self.log.info(u'获取%s信息: 成功' % item.name)
+            except AttributeError:
+                self.log.info(u'url None')
         return items
     
     def pipelines(self):
-        #数据清洗保存
-        filename = 'daxuedata.txt'
+        filename = 'newdata.txt'
         with codecs.open(filename, 'w', 'utf8') as fp:
             for item in self.items:
-                fp.write('%d \t %s \t %s \t %.f \n' % (int(item.paihang), item.name, item.address, float(item.score)))
+                fp.write('%s \t %s \n' % (item.name, item.content))
                 self.log.info(u'%s保存至%s:成功' % (item.name, filename))
 
 if __name__ == '__main__':
